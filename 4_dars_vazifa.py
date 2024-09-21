829000ecdbd72cb4dd983951c3f5d5307df1657e
Tavakkaljon995 <tavakkaljonrafiqov@gmail.com>
2024-09-21 12:11:33 +0500

    Create 4_dars_vazifa.py

---
+++ 4_dars_vazifa.py
@@ -0,0 +1,14 @@
+salom = 'Hello World'
+print(salom)
+
+xabar = "Bugun havo 23 gradus"
+print(xabar)
+xabar = "Bugun havo 45 gradus"
+print(xabar)
+
+# class deb o'zgaruvchi yaratib bo'lmaydi, chunki bu keywords hisoblanadi
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
