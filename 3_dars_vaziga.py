705e2d5e862c1cf14d442878edc4162503a78b50
Tavakkaljon995 <tavakkaljonrafiqov@gmail.com>
2024-09-21 11:55:21 +0500

    3-dars-vazifa

---
+++ 3_dars_vaziga.py
@@ -0,0 +1,15 @@
+print('"Nexia", "Tico", \'Damas\' ko\'rganlar qilar havas')
+
+print(5**4)
+
+print(22%4)
+
+tomon = 125
+print(f"Tomonlari 125 bo'lgan kvadratning yuzi {125**2}, perimetri esa {125*4} ")
+
+diametr = 12
+pi = 3.14
+print(f"Diametri 12 bo'lgan doiraning yuzi {pi*(diametr/2)**2}")
+
+x,y = 6,7
+print('Gipotenuza= ', (x**2 + y**2)**0.5)
